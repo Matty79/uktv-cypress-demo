09568014bb2fb032a8bdc70b411e570e8882ab44
Matt Vickers <matt.vickers@ecs.co.uk>
2020-01-22 17:36:37 +0000

    first commit

---
+++ cypress/integration/examples/login.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+describe("Login", () => {
+  context("iPhone 6", () => {
+    beforeEach("visit UKTV website using iPhone 6", () => {
+      // resolution and baseUrl specified in cypress.json
+      cy.visit("/");
+      // password details specified in fixtures/uktvuser.json
+      cy.fixture("uktvuser").as("user");
+    });
+
+    it("displays mobile menu when clicked", () => {
+      cy.get("navigation-bar transparent").should("not.be.visible");
+      cy.get(".menu-btn")
+        .should("be.visible")
+        .click();
+      cy.contains("Sign in").should("be.visible");
+    });
+
+    it("signs user in", () => {
+      cy.get(".menu-btn").click();
+      cy.contains("Sign in").click();
+      cy.get("@user").then(user => {
+        cy.get("#email").type(user.username);
+        cy.get("#password").type(user.password);
+      });
+      cy.get("#sign-in-btn").click();
+    });
+  });
+});
---
+++ cypress/integration/examples/search.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe("Search", () => {
+  context("iPhone 6 resolution", () => {
+    beforeEach("visit UKTV website using iPhone 6 and dismiss cookie notice", () => {
+      cy.visit("/");
+      cy.fixture("uktvsearch").as("search");
+      cy.get('.btn-container > .accept').click();
+    });
+
+    it("displays relevant search result", () => {
+      cy.get(".nav-buttons")
+        .should("be.visible")
+        .click();
+      cy.get("@search").then(search => {
+        cy.get(".search-input")
+          .should("be.visible")
+          .type(search.search1);
+        cy.get(".results-list > :nth-child(1)").click();
+        // need to match search term somehow taking into account capitalisation variation
+        cy.get(".vod-episode__title".toUpperCase).should("eq", (search.search1.toUpperCase));
+      });
+    });
+  });
+});
